d65bdec0e0f36245bff61a590a89ecaac0a87fab
gangx <gangx6906@gmail.com>
2024-04-29 19:56:09 +0800

    为engine添加数据库驱动对应的方言属性

--- engine.go
+++ engine.go
@@ -2,12 +2,14 @@ package mygoorm
 
 import (
 	"database/sql"
+	"my_orm/dialect"
 	"my_orm/log"
 	"my_orm/session"
 )
 
 type Engine struct {
-	db *sql.DB
+	db      *sql.DB
+	dialect dialect.Dialect
 }
 
 // 创建engine根据数据库的驱动类型以及数据库的资源路径
@@ -25,14 +27,18 @@ func NewEngine(driver, source string) (*Engine, error) {
 		log.Error(err)
 		return nil, err
 	}
-	e := &Engine{db: db}
+	dialect, ok := dialect.GetDialect(driver)
+	if !ok {
+		log.Error("dialect %s Not Found", driver)
+	}
+	e := &Engine{db: db, dialect: dialect}
 	log.Info("db conn success")
 	return e, nil
 }
 
 // 根据engine创建会话
 func (e *Engine) CreateSession() *session.Session {
-	return session.NewSession(e.db)
+	return session.NewSession(e.db, e.dialect)
 }
 func (e *Engine) Close() {
 	err := e.db.Close()
